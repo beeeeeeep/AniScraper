28b3b3c8350067de66112b3571671dab61968816
Beep Beep <ivs.spamacc@gmail.com>
2021-02-10 23:10:06 +0000

    Added better logging, scraped cache, loop

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ MIN_SERIES_SIZE = 3  # the minimum accepted size of TV shows in GiB
 INDEXER_KEYWORDS = ["blu-ray", "blu ray", "(bd", "[bd", "bdrip"]  # results containing these in the title get a rank boost
 INDEXER_QUALITY = "1080p"  # preferred quality. Not a hard limit.
 INDEXER_GROUPS = ["commie", "erai-raws", "horriblesubs", "subsplease"]  # preferred release groups
+INTERVAL = 10 * 60  # in seconds
--- main.py
+++ main.py
@@ -4,113 +4,142 @@ from implementations.search.anilist import search
 from implementations.indexer.nyaa import indexer
 from implementations.torrent.deluged import deluged
 from indexer import Indexer
-from config import INDEXER_GROUPS, MEDIA_DIR_SERIES, MEDIA_DIR_FILMS, TORRENT_DIR, DISABLE_NEW_ANIME, ACCOUNT, MIN_SERIES_SIZE, INDEXER_KEYWORDS, INDEXER_QUALITY
+from config import INDEXER_GROUPS, INTERVAL, MEDIA_DIR_SERIES, MEDIA_DIR_FILMS, TORRENT_DIR, DISABLE_NEW_ANIME, ACCOUNT, MIN_SERIES_SIZE, INDEXER_KEYWORDS, INDEXER_QUALITY
 import os
 import json
 import time
 import re
 import requests
+import sched
+import logging
 from torrentool.torrent import Torrent
 
 
-# Lookup IDs of already downloaded stuff
-prepare_dir(MEDIA_DIR_FILMS, search)
-prepare_dir(MEDIA_DIR_SERIES, search)
+s = sched.scheduler(time.time, time.sleep)
 
-# Get IDs already in storage
-with open("anime_ids.json") as fp:
-    data = json.load(fp)
-    series = data[MEDIA_DIR_SERIES]
-    films = data[MEDIA_DIR_FILMS]
-    blacklist = data.get("blacklist")  # To avoid repeating failing searches
-    if blacklist is None:
-        data["blacklist"] = []
-        blacklist = data["blacklist"]
+def once():
+    logging.info("Running scrape")
 
-# Get plan to watch list
-# format: List[Tuple[title, id, type, year]]
-ptw_anime = ptw.fetch(ACCOUNT)
+    # Get IDs already in storage
+    with open("anime_ids.json") as fp:
+        data = json.load(fp)
+        series = data[MEDIA_DIR_SERIES]
+        films = data[MEDIA_DIR_FILMS]
+        blacklist = data.get("blacklist")  # To avoid repeating failing searches
+        if blacklist is None:
+            data["blacklist"] = []
+            blacklist = data["blacklist"]
 
-for anime in ptw_anime:
-    anime_title = re.sub(r" +", " ", re.sub(r"[^0-9a-zA-Z\ \-\&]+", " ", anime[0])).strip()
-    anime_id = str(anime[1])
-    anime_type = anime[2]
-    anime_year = int(anime[3])
+    # Previously scraped anime IDs. This uses the PTW IDs to avoid waiting for anilist search IDs.
+    with open("scraped.json") as fp:
+        scraped = json.load(fp)
 
-    # Get anilist ID
-    search_id = search.fetch(anime_title)
-    if search_id is None:
-        print(f"WARNING: ID search for {anime_title} returned None.")
-        continue
-    time.sleep(1)  # avoid anilist rate limit
+    # Get plan to watch list
+    # format: List[Tuple[title, id, type, year]]
+    ptw_anime = ptw.fetch(ACCOUNT)
 
-    if search_id in blacklist:
-        continue
-    if anime_type == "Movie" and search_id in films.values():
-        continue
-    elif search_id in series.values():
-        continue
+    for anime in ptw_anime:
+        anime_title = re.sub(r" +", " ", re.sub(r"[^0-9a-zA-Z\ \-\&]+", " ", anime[0])).strip()
+        anime_id = str(anime[1])
+        anime_type = anime[2]
+        anime_year = int(anime[3])
 
-    if anime_year >= 2020 and DISABLE_NEW_ANIME:
-        print(f"INFO: {anime_title} is newer than 2020. Adding to blacklist.")
-        blacklist.append(search_id)
-        continue
+        if anime_id in scraped:
+            continue
 
-    # Query the indexer
-    indexer_query = indexer.query(anime_title)
+        scraped.append(anime_id)
 
-    if len(indexer_query) == 0:
-        error_msg = f"INFO: could not find anime with title {anime_title} on indexer. Adding to blacklist."
-        print(error_msg)
-        blacklist.append(search_id)
-        continue
-    top_ranks = Indexer.rank(
-        indexer_query, 
-        pref_groups=INDEXER_GROUPS, 
-        pref_quality=INDEXER_QUALITY, 
-        keywords=INDEXER_KEYWORDS,
-        type=anime_type,
-        min_gib=MIN_SERIES_SIZE if anime_type == "TV" else None,  #TODO: this is quite jank, fix
-        prefer_first_season=True
-    )
+        # Get anilist ID
+        search_id = search.fetch(anime_title)
+        if search_id is None:
+            logging.warning(f"ID search for {anime_title} returned None.")
+            continue
+        time.sleep(1)  # avoid anilist rate limit
 
-    if len(top_ranks) == 0:
-        error_msg = f"INFO: Could not find a suitable batch for {anime_title}."
-        if anime_year > 2018:
-            error_msg += "This anime aired in 2019 or later, so it is possible it doesn't yet have batches. Try adding it on Sonarr."
-        print(error_msg)
-        continue
+        if search_id in blacklist:
+            continue
+        if anime_type == "Movie" and search_id in films.values():
+            continue
+        elif search_id in series.values():
+            continue
 
-    torrent_url = top_ranks[0]["link"]
+        if anime_year >= 2020 and DISABLE_NEW_ANIME:
+            logging.info(f"{anime_title} is newer than 2020. Adding to blacklist.")
+            blacklist.append(search_id)
+            continue
 
-    # GET .torrent file, parse and get torrent file name
-    r = requests.get(torrent_url)
-    if r.status_code != 200:
-        raise ConnectionError("Failed to fetch torrent file")
-    torrent_file_name = Torrent.from_string(r.content).name
+        # Query the indexer
+        indexer_query = indexer.query(anime_title)
 
-    # Add torrent using url
-    success = deluged.execute("add", ("-p", TORRENT_DIR), (None, torrent_url))
+        if len(indexer_query) == 0:
+            error_msg = f"could not find anime with title {anime_title} on indexer. Adding to blacklist."
+            logging.info(error_msg)
+            blacklist.append(search_id)
+            continue
+        top_ranks = Indexer.rank(
+            indexer_query, 
+            pref_groups=INDEXER_GROUPS, 
+            pref_quality=INDEXER_QUALITY, 
+            keywords=INDEXER_KEYWORDS,
+            type=anime_type,
+            min_gib=MIN_SERIES_SIZE if anime_type == "TV" else None,  #TODO: this is quite jank, fix
+            prefer_first_season=True
+        )
 
-    if not success:
-        raise RuntimeError("Torrent client error")
-    if anime_type == "Movie":
-        new_file_dir = MEDIA_DIR_FILMS
-        films[anime_title] = search_id
-    else:
-        new_file_dir = MEDIA_DIR_SERIES
-        series[anime_title] = search_id
-    os.mkdir(new_file_dir + anime_title)
-    if any(torrent_file_name.endswith(x) for x in [".mp4", ".mkv"]):
-        new_filepath = "/" + torrent_file_name
-    else:
-        new_filepath = "/Season 1"
-    os.symlink(TORRENT_DIR + torrent_file_name, new_file_dir + anime_title + new_filepath)
-    print(f"Added ({anime_type}) {anime_title}")
+        if len(top_ranks) == 0:
+            error_msg = f"INFO: Could not find a suitable batch for {anime_title}."
+            if anime_year > 2018:
+                error_msg += "This anime aired in 2019 or later, so it is possible it doesn't yet have batches. Try adding it on Sonarr."
+            logging.info(error_msg)
+            continue
 
+        torrent_url = top_ranks[0]["link"]
+
+        # GET .torrent file, parse and get torrent file name
+        r = requests.get(torrent_url)
+        if r.status_code != 200:
+            raise ConnectionError("Failed to fetch torrent file")
+        torrent_file_name = Torrent.from_string(r.content).name
+
+        # Add torrent using url
+        success = deluged.execute("add", ("-p", TORRENT_DIR), (None, torrent_url))
+
+        if not success:
+            raise RuntimeError("Torrent client error")
+        if anime_type == "Movie":
+            new_file_dir = MEDIA_DIR_FILMS
+            films[anime_title] = search_id
+        else:
+            new_file_dir = MEDIA_DIR_SERIES
+            series[anime_title] = search_id
+        os.mkdir(new_file_dir + anime_title)
+        if any(torrent_file_name.endswith(x) for x in [".mp4", ".mkv"]):
+            new_filepath = "/" + torrent_file_name
+        else:
+            new_filepath = "/Season 1"
+        os.symlink(TORRENT_DIR + torrent_file_name, new_file_dir + anime_title + new_filepath)
+        logging.info(f"Added ({anime_type}) {anime_title}")
+
+        with open("anime_ids.json", "w") as fp:
+            json.dump(data, fp, indent=4)
+
+    # TODO: maybe unjank this 
     with open("anime_ids.json", "w") as fp:
         json.dump(data, fp, indent=4)
 
-# TODO: maybe unjank this 
-with open("anime_ids.json", "w") as fp:
-    json.dump(data, fp, indent=4)
+    with open("scraped.json", "w") as fp:
+        json.dump(scraped, fp, indent=4)
+
+    logging.info("Scrape finished")
+
+    s.enter(INTERVAL, 1, once)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
+
+    # Lookup IDs of already downloaded stuff
+    prepare_dir(MEDIA_DIR_FILMS, search)
+    prepare_dir(MEDIA_DIR_SERIES, search)
+
+    s.enter(0, 1, once)
+    s.run()
