452a4683bdb45f22a2311eec68f0ee14ef34f169
Beep Beep <ivs.spamacc@gmail.com>
2021-03-08 20:52:03 +0000

    Added docker env config

---
+++ copy_config.py
@@ -0,0 +1,9 @@
+import re
+
+
+with open("./config.py") as fp:
+    data = fp.readlines()
+
+data = [re.sub(r" ?= ?", "=", re.sub(r"\#.*$", "", x)) for x in data if any(y in x for y in ["MEDIA_DIR_SERIES", "MEDIA_DIR_FILMS", "TORRENT_DIR"])]
+with open("./docker_config.env", "w") as fp:
+    fp.write("\n".join(data))
