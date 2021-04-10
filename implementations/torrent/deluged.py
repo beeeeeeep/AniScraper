39892e5d63e32c05bb0e5bb91343511331ec3fec
beeeeeeep <ivs.spamacc@gmail.com>
2021-04-10 12:21:30 +0100

    Commented out deluged implementation

--- implementations/torrent/deluged.py
+++ implementations/torrent/deluged.py
@@ -1,11 +1,11 @@
-from service_classes.torrent_client import ShellProgram, BinaryOperator
+# from service_classes.torrent_client import ShellProgram, BinaryOperator
 
-torrent = ShellProgram(
-    command_name="deluge-console",
-    params=[],
-    operators={
-        "add": BinaryOperator(lambda arg1, arg2: ["add", arg1, "-p", arg2])
-    },
-    error_strings=["Torrent was not added"],
-    success_strings=["Torrent added!"]
-)
+# torrent = ShellProgram(
+#     command_name="deluge-console",
+#     params=[],
+#     operators={
+#         "add": BinaryOperator(lambda arg1, arg2: ["add", arg1, "-p", arg2])
+#     },
+#     error_strings=["Torrent was not added"],
+#     success_strings=["Torrent added!"]
+# )
